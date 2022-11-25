00e4d095f1b046f85c0648325bf849adf88f44c6
13761566975 <q19891025>
2022-11-25 22:42:17 +0800

    底部样式调整

--- src/bottombar/BottomBarComponent.jsx
+++ src/bottombar/BottomBarComponent.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './bottombar.css'
+import RedditUrl from '../images/reddit.png'
+import TwitterUrl from '../images/twitter.png'
 
 export default function useBottomBarComponent() {
   return (
     <div className='bottomBg'>
-      
+        <div className='bottomIcon'>
+            <a href="https://twitter.com/twitter链接" target="_blank" className="join_item">
+                <img src={RedditUrl} loading="lazy" alt="Reddit"/>
+            </a>
+            <a href="https://twitter.com/twitter链接" target="_blank" className="join_item">
+                <img src={TwitterUrl} loading="lazy" alt="Twitter"/>
+            </a>
+        </div>
     </div>
   )
 }
