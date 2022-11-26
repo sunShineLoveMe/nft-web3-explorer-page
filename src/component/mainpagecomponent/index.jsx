32c607158b363be188a39c1196b42be4e569dcb5
13761566975 <q19891025>
2022-11-26 15:16:40 +0800

    主页内容提交

---
+++ src/component/mainpagecomponent/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+
+export default function useMainPageComponent() {
+    return (
+        <div className='main-page'>
+            <h1 className='overview-title'>
+                项目介绍
+            </h1>
+            <p className='overview-text'>
+                白皮书内容
+            </p>
+        </div>
+    )
+}
